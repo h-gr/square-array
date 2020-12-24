36a24fa5034457f34aa50ad40feda5b8e5c4fc2f
unknown <hugoalexgr@gmail.com>
2020-12-24 13:33:00 +0100

    trace added

--- src/main.rs
+++ src/main.rs
@@ -207,6 +207,7 @@ fn full_sequential_merge(v : &[i64], buffer: &mut[i64])
 
 fn full_parallel_merge(v : &[i64], mut buffer:  Vec<i64>)
 {
+    fast_tracer::svg("array_sq.svg", || {
     let start = std::time::Instant::now();
     let index = binary_search(&v, 0, false); //find the index of 0
     let (neg, pos) = v.split_at(index); //split into negative and positive
@@ -225,14 +226,14 @@ fn full_parallel_merge(v : &[i64], mut buffer:  Vec<i64>)
         seq_merge(&neg[aat.0..aat.1], &pos[bb.0..bb.1], cc)
     });
     println!("par: {:?}", start.elapsed());
+    })
+    .expect("failed saving svg file");
     assert!(buffer[buffer.len()-1]!=0);
     assert!(buffer.windows(2).all(|w| w[0] <= w[1]));
 }
 
-
-
 fn main() {
-    rayon::ThreadPoolBuilder::new().num_threads(8).build_global().unwrap();
+    //rayon::ThreadPoolBuilder::new().num_threads(8).build_global().unwrap();
     let mut rng = rand::thread_rng();
     let v: Vec<i64> = (0..N).map(|_| {
         rng.gen_range(-N/2, N/2)}).sorted().collect();
@@ -243,6 +244,7 @@ fn main() {
     full_sequential_merge(&v,&mut buffer);
     //parallel version
     buffer.iter_mut().for_each(|x| *x = 0); //we reset the buffer
+
     full_parallel_merge(&v, buffer);
     
 }
